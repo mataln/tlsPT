a0484a1c6d9b4d835203f82d7ce6579d9c03db7c
mja78@cam.ac.uk <mja78@cam.ac.uk>
2024-11-28 23:44:43 +0000

    Finished seg model not tested

--- src/tlspt/models/pointmae/pointmae_seg.py
+++ src/tlspt/models/pointmae/pointmae_seg.py
@@ -1,15 +1,20 @@
 from __future__ import annotations
 
+import math
+
 import lightning as L
 import torch
 from loguru import logger
-from pytorch3d.loss import chamfer_distance
 from torch import nn
 
 from tlspt.models.transformer import TransformerEncoder
-from tlspt.models.utils import get_at_index
 
-from .components import Group, PointNetEncoder, PositionEncoder
+from .components import (
+    Group,
+    PointNetEncoder,
+    PointNetFeaturePropagation,
+    PositionEncoder,
+)
 
 
 class PointMAE(L.LightningModule):
@@ -33,6 +38,8 @@ class PointMAE(L.LightningModule):
             "drop_path_rate": 0.1,
         },
         feature_blocks: list = [3, 7, 11],
+        num_points: int = 1024,
+        cls_dim: int = 2,  # Leaf/Wood
         total_epochs: int = 300,
     ):
         super().__init__()
@@ -72,9 +79,25 @@ class PointMAE(L.LightningModule):
             self.transformer_encoder = TransformerEncoder(**self.transencoder_config)
             self.norm = nn.LayerNorm(self.trans_dim)
 
-        self.loss = XX
+        self.loss = nn.CrossEntropyLoss()
+        self.cls_dim = cls_dim
         self.total_epochs = total_epochs
         self.feature_blocks = feature_blocks
+
+        # Decoder
+        self.propagation_0 = PointNetFeaturePropagation(
+            in_channel=len(feature_blocks) * self.embedding_dim + 3,
+            mlp=[self.embedding_dim * 4, self.num_points],
+        )
+
+        self.convs1 = nn.Conv1d(3392, 512, 1)
+        self.dp1 = nn.Dropout(0.5)
+        self.convs2 = nn.Conv1d(512, 256, 1)
+        self.convs3 = nn.Conv1d(256, self.cls_dim, 1)
+        self.bns1 = nn.BatchNorm1d(512)
+        self.bns2 = nn.BatchNorm1d(256)
+
+        self.relu = nn.ReLU()
 
     def reconstruct(
         self, x
@@ -113,34 +136,74 @@ class PointMAE(L.LightningModule):
         return x_hat
 
     def forward(self, x):
+        B, N, _ = x[
+            "points"
+        ].shape  # B: batch size, N: number of points, _: number of dimensions (3)
+
+        x_gt = x["features"]  # Ground truth points (batch, N, 1) #Cls labels
+        if x["features"].shape[2] > 1:
+            raise ValueError(
+                "Multiple features not supported. Need to use label as only feature."
+            )
+        x_gt = x_gt.squeeze(-1)  # (batch, N)
+
         patches, centers = self.group(
             x["points"], x["lengths"]
         )  # patches (batch, no centers, no neighbors, 3), centers (batch, no centers, 3)
 
         # Encode
-        x, pos_embeddings, feature_tensor = self.forward_encoder(
+        x, pos_embeddings, feature_list = self.forward_encoder(
             patches, centers
-        )  # x: (batch, centers, transformer_dim), pos_embeddings: (batch, centers, transformer_dim), feature_tensor: (no. blocks, B, no. centers, transformer dim)
+        )  # x: (B, centers, transformer_dim), pos_embeddings: (batch, centers, transformer_dim), feature_tensor: no groups long list of (B, no. centers, transformer dim)
 
-        # Decode
-        x_hat = self.forward_decoder(
-            x_full, full_pos_embeddings, N
-        )  # Decode to patch embeddings w/ missing patches, (batch, no mask centers, no neighbors, 3)
-        x_gt = get_at_index(patches, masked_idx)
+        # Stack feature list
+        feature_tensor = torch.cat(
+            feature_list, dim=-1
+        )  # (B, no. centers, no. groups * transformer dim)
+        x_max = torch.max(feature_tensor, dim=1)[
+            0
+        ]  # Max along centers, (B, no. groups * transformer dim)
+        x_avg = torch.mean(
+            feature_tensor, dim=1
+        )  # Average along centers, (B, no. groups * transformer dim)
+
+        x_max_feature = x_max.unsqueeze(1).repeat(
+            1, 1, N
+        )  # (B, 1, no. groups * transformer dim) -> (B, N, no. groups * transformer dim)
+        x_avg_feature = x_avg.unsqueeze(1).repeat(
+            1, 1, N
+        )  # (B, 1, no. groups * transformer dim) -> (B, N, no. groups * transformer dim)
+
+        x_global_feature = torch.cat(
+            [x_max_feature, x_avg_feature], dim=-1
+        )  # (B, N, 2 * no. groups * transformer dim)
+
+        f_level_0 = self.propagation_0(
+            x["points"], centers, x["points"], feature_tensor
+        )  # (B, N, D')
+        x = torch.cat(
+            (f_level_0, x_global_feature), dim=-1
+        )  # (B, N, D' + 2 * no. groups * transformer dim)
+
+        # Transpose for conv1d
+        x = x.transpose(1, 2)
+
+        x = self.relu(self.bns1(self.convs1(x)))
+        x = self.dp1(x)
+        x = self.relu(self.bns2(self.convs2(x)))
+        x = self.convs3(x)
+
+        x_hat = x.transpose(1, 2)  # (B, N, cls_dim)
 
         loss = self.get_loss(x_hat, x_gt)
         return loss
 
     def get_loss(self, x_hat, target):
-        B, M, N, _ = x_hat.shape
-        x_hat = x_hat.reshape(B * M, N, 3)
-        target = target.reshape(B * M, N, 3)
+        B, N, cls_dim = x_hat.shape
+        x_hat = x_hat.reshape(B * N, cls_dim)
+        target = target.reshape(B * N)
 
-        if self.loss == chamfer_distance:
-            loss = self.loss(x_hat, target)[0]
-        else:
-            raise NotImplementedError
-        return loss
+        return self.loss(x_hat, target)
 
     def training_step(self, batch, batch_idx):
         loss = self.forward(batch)
@@ -162,16 +225,24 @@ class PointMAE(L.LightningModule):
         self.logger.experiment.log(self.hparams)
 
     def configure_optimizers(self):
-        optimizer = torch.optim.AdamW(self.parameters(), lr=0.0001, weight_decay=0.05)
-        return optimizer
-        # total_epochs = 300
-        # warmup_epochs = 10
+        optimizer = torch.optim.AdamW(self.parameters(), lr=0.001, weight_decay=0.05)
+        # return optimizer
+        warmup_epochs = 10
 
-        # def lr_lambda(current_epoch):
-        #     if current_epoch < warmup_epochs:
-        #         return float(current_epoch) / float(max(1, warmup_epochs))
-        #     else:
-        #         return 0.5 * (1 + math.cos(math.pi * (current_epoch - warmup_epochs) / (total_epochs - warmup_epochs)))
+        def lr_lambda(current_epoch):
+            if current_epoch < warmup_epochs:
+                return float(current_epoch) / float(max(1, warmup_epochs))
+            else:
+                return 0.5 * (
+                    1
+                    + math.cos(
+                        math.pi
+                        * (current_epoch - warmup_epochs)
+                        / (self.total_epochs - warmup_epochs)
+                    )
+                )
 
-        # scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lr_lambda)
-        # return [optimizer], [{'scheduler': scheduler, 'interval': 'epoch', 'frequency': 1}]
+        scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lr_lambda)
+        return [optimizer], [
+            {"scheduler": scheduler, "interval": "epoch", "frequency": 1}
+        ]
